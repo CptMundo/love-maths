767705f1491be9709493efbf7c55efa9d60086a7
CptMundo <>
2023-08-22 20:25:58 +0000

    Start JavaScript to be continued

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,46 @@
-console.log("Hello!!");
+//wait for the DOM to finish loading before running the game
+//get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons ) document.getElementsByTagName("button");
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        if (this.getAtribute("data-type") === "submit") {
+            alert("You clicked submit!");
+        }
+    });
+}
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculatorCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
